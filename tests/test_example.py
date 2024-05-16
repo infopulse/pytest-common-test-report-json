dbe76b91fdf78670f01d74a5f37c06420f266efe
ypurek <oleksii.ostapov@gmail.com>
2024-05-16 15:49:17 +0300

    base structure created

---
+++ tests/test_example.py
@@ -0,0 +1,18 @@
+def test_1():
+    assert 2 + 2 == 4
+
+
+def test_2():
+    assert 3 + 3 == 6
+
+
+def test_3():
+    assert 4 + 4 == 8
+
+
+def test_4():
+    assert 5 + 5 == 10
+
+
+def test_5():
+    assert 6 + 6 == 12
